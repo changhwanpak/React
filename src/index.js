dc030e2de0be14b9d065224c815626709c3b6019
changhwanpak <ppch0416@gmail.com>
2024-08-29 16:26:27 +0900

    React 1장,2장

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import'chp03/Book.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from "./chp03/Library";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
